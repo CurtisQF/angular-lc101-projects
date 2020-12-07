f346516a6bd2bd8bb0d2c72aa634ac332b460724
Curtis Moeller <curtisqfarrell@gmail.com>
2020-12-07 16:21:09 -0600

    First commit

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Photos Section';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKnSF1i6kr3SfCg150kXY6-7OVGg6D9ad9Bg&usqp=CAU';
+  image3 = 'https://lh3.googleusercontent.com/proxy/rzQhIgJF00cz4hhLo2t3AJb2dMSQ7usOmQiB9BiJJhWCEUWkDc5qrzEmvXZvNG4zSp5C0Ui8N7XlGknC2um_iaFWQ8qNrfg';
 
   constructor() { }
 
